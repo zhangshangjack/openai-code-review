5289d604462863ba458ab6ed3728cfd48807c11f
shanezhang <shanezhang@futurefab.ai>
2025-05-08 14:24:13 +0800

    feat: 代码检出

--- openai-code-review-sdk/src/main/java/cn/zs/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/cn/zs/middleware/sdk/OpenAiCodeReview.java
@@ -1,4 +1,8 @@
 package cn.zs.middleware.sdk;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+
 
 /**
  * @Description:
@@ -6,7 +10,30 @@ package cn.zs.middleware.sdk;
  * @Date: 2025/05/07/14:40
  */
 public class OpenAiCodeReview {
-    public static void main(String[] args) {
+   
+    public static void main(String[] args) throws Exception {
         System.out.println("测试执行");
+        
+
+        // 1. 代码检出
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
+        processBuilder.directory(new File("."));
+
+        Process process = processBuilder.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+
+        StringBuilder diffCode = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            diffCode.append(line);
+        }
+
+        int exitCode = process.waitFor();
+        System.out.println("Exited with code:" + exitCode);
+
+        System.out.println("评审代码：" + diffCode.toString());
     }
+
+
 }
