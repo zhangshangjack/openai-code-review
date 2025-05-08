fffc7e9ff85e114c4ed5be14789b204c0fa12263
shanezhang <shanezhang@futurefab.ai>
2025-05-08 16:23:51 +0800

    feat: 评审写入日志 v5.1

--- openai-code-review-test/src/test/java/cn/zs/middleware/sdk/test/ApiTest.java
+++ openai-code-review-test/src/test/java/cn/zs/middleware/sdk/test/ApiTest.java
@@ -18,7 +18,7 @@ public class ApiTest {
 
     @Test
     public void test() {
-        System.out.println(Integer.parseInt("aaa1234"));
+        System.out.println(Integer.parseInt("abc1234"));
 
     }
 }
