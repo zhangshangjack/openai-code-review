054c641f72d58fc571ab25e0a97324cfd533ec20
shanezhang <shanezhang@futurefab.ai>
2025-05-09 11:00:21 +0800

    fear: 代码重构 v7.1

--- openai-code-review-test/src/test/java/cn/zs/middleware/sdk/test/ApiTest.java
+++ openai-code-review-test/src/test/java/cn/zs/middleware/sdk/test/ApiTest.java
@@ -18,7 +18,7 @@ public class ApiTest {
 
     @Test
     public void test() {
-        System.out.println(Integer.parseInt("ddd222"));
+        System.out.println(Integer.parseInt("1223"));
 
     }
 }
